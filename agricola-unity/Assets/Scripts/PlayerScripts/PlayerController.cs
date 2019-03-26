27fea4907ad014838cc56deccb91be94cf933336
Budziaszek <32570679+Budziaszek@users.noreply.github.com>
2019-03-26 14:41:22 +0100

    Action queque basic visualization

--- agricola-unity/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ agricola-unity/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -13,6 +13,12 @@ public class PlayerController : MonoBehaviour
     public NavMeshAgent agent;
     public int health;
     public int maxHealth;
+    public int hunger;
+    public int maxHunger;
+    public int quequeVisualPosition;
+    public int queueVisualInterspace;
+    public Vector2 queueVisualElementSize;
+    public List<Button> listVisualElements;
 
     private Queue<Vector3> actionQueue; // poczatkowo tylko wektor pozycji
     private bool doActionPressed = false;
@@ -26,26 +32,43 @@ public class PlayerController : MonoBehaviour
         agent.speed = 6f; // test
         health = 100;
         maxHealth = 100;
+        hunger = 50;
+        maxHunger = 50;
+        quequeVisualPosition = 7;
+        queueVisualInterspace = 14;
+        queueVisualElementSize = new Vector2(20, 20);
         actualizeHealthBar();
+        actualizeHungerBar();
     }
 
     void actualizeHealthBar()
     {
-        Image bar = GameObject.Find("Bar").GetComponent<Image>();
+        Image bar = GameObject.Find("HealthBar").GetComponent<Image>();
         bar.rectTransform.localScale = new Vector2(health / maxHealth, 1f);
+        Text value = GameObject.Find("HealthValue").GetComponent<Text>();
+        value.text =  health.ToString() + "/" + maxHealth.ToString();
+    }
+
+    void actualizeHungerBar()
+    {
+        Image bar = GameObject.Find("HungerBar").GetComponent<Image>();
+        bar.rectTransform.localScale = new Vector2(hunger / maxHunger, 1f);
+        Text value = GameObject.Find("HungerValue").GetComponent<Text>();
+        value.text = hunger.ToString() + "/" + maxHunger.ToString();
+        //if lower than... do...
     }
 
     // once per frame
     void Update()
     {
-        addPointedAction();
-
-
         if (doActionPressed)
         {
             doAction();
         }
-
+        else
+        {
+            addPointedAction();
+        }
     }
 
 
@@ -59,6 +82,7 @@ public class PlayerController : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
                 actionQueue.Enqueue(hit.point);
+                addQuequeElement();
             }
         }
     }
@@ -67,20 +91,25 @@ public class PlayerController : MonoBehaviour
     {
         if (isPathFinished())
         {
-            if(actionQueue.Count > 0)
+            if (actionQueue.Count > 0) { 
                 agent.SetDestination(actionQueue.Dequeue());
-        }
-
-        if (actionQueue.Count == 0)
-        {
-            doActionPressed = false;
+                removeQuequeElement(0);
+            }
+            else
+            {
+                doActionPressed = false;
+                Button playButton = GameObject.Find("PlayButton").GetComponent<Button>();
+                playButton.interactable = true;
+                quequeVisualPosition = 7;
+            }
         }
     }
 
 
     public void StartActionQueue()
     {
-
+        Button playButton = GameObject.Find("PlayButton").GetComponent<Button>();
+        playButton.interactable = false;
         doActionPressed = true;
 
     }
@@ -101,6 +130,43 @@ public class PlayerController : MonoBehaviour
         return false;
     }
 
+    public void addQuequeElement()
+    {
+        GameObject mCanvas = GameObject.Find("Canvas");
+        GameObject button = new GameObject();
+
+        button.AddComponent<CanvasRenderer>();
+        button.AddComponent<RectTransform>();
+        button.layer = 5;
+        Button mButton = button.AddComponent<Button>();
+        Image mImage = button.AddComponent<Image>();
+        mButton.targetGraphic = mImage;
+        button.transform.SetParent(mCanvas.transform);
+
+
+        RectTransform rectTransform = button.GetComponent<RectTransform>();
+        rectTransform.localPosition = new Vector3(quequeVisualPosition + queueVisualInterspace/2, -queueVisualInterspace, 0);
+        rectTransform.sizeDelta = queueVisualElementSize;
+        rectTransform.anchorMin = new Vector2(0, 1);
+        rectTransform.anchorMax = new Vector2(0, 1);
+
+        quequeVisualPosition += queueVisualInterspace/2 + (int)queueVisualElementSize.x;
+
+        listVisualElements.Add(mButton);
+    }
+
+    public void removeQuequeElement(int i)
+    {
+        Destroy(listVisualElements[i].gameObject);
+        listVisualElements.RemoveAt(i);
+        for(int j = i; j< listVisualElements.Count; j++)
+        {
+            int move = queueVisualInterspace / 2 + (int)queueVisualElementSize.x;
+            RectTransform rectTransform = listVisualElements[j].GetComponent<RectTransform>();
+            rectTransform.localPosition = new Vector3(rectTransform.localPosition.x - move,rectTransform.localPosition.y, 0);
+        }
+        
+    }
 }
 
 
