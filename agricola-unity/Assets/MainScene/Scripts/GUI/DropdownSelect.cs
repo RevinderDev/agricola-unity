c603b3aff30f11dd829a2779af8664e33bcdad08
Budziaszek <32570679+Budziaszek@users.noreply.github.com>
2019-04-28 14:30:14 +0200

    New seeds select menu

--- agricola-unity/Assets/MainScene/Scripts/GUI/DropdownSelect.cs
+++ agricola-unity/Assets/MainScene/Scripts/GUI/DropdownSelect.cs
@@ -1,66 +1,51 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class DropdownSelect : MonoBehaviour
+public class DropdownSelect : EventTrigger
 {
-    public static List<string> dropOptions = new List<string> { "select seeds", "carrot seeds", "tomato seeds" };
-    private Dropdown dropdown;
+    public GameObject dropdown;
     private string selected;
     GameController gameController;
     GameObject areaObject;
-    Rect dropdownRectClosed;
-    Rect dropdownRectOpened;
-
 
     void Start()
     {
         gameController = GameObject.Find("GameController").GetComponent<GameController>();
-        dropdown = GetComponent<Dropdown>();
-        dropdown.ClearOptions();
-        dropdown.AddOptions(dropOptions);
-        dropdown.onValueChanged.AddListener(delegate {
-            DropdownValueChanged(dropdown);
-        });
+        dropdown = GameObject.Find("Dropdown");
     }
 
-    public string getSelected()
+    public void OnOptionSelected(string selected)
+    {
+        this.selected = selected;
+        gameController.AddAction(areaObject, ActionType.plant);
+        ActionController.isActive = true;
+        Hide();
+    }
+
+    public string GetSelected()
     {
         return selected;
     }
 
-    void DropdownValueChanged(Dropdown change)
-    {
-        if (dropdown.value != 0)
-        {
-            selected = dropOptions[dropdown.value];
-            gameController.AddAction(areaObject, ActionType.plant);
-            Hide();
-        }
-    }
-
     public void Hide()
     {
+        dropdown.SetActive(false);
         ActionController.isActive = true;
-        dropdown.gameObject.SetActive(false);
-        if(dropdown.transform.Find("Dropdown List") != null)
-            Destroy(dropdown.transform.Find("Dropdown List").gameObject);
     }
 
     public void Display(GameObject areaObject)
     {
         this.areaObject = areaObject;
-        dropdown.ClearOptions();
-        dropdown.AddOptions(dropOptions);
-        dropdown.value = 0;
         var x = Input.mousePosition.x;
         var y = Input.mousePosition.y;
-        dropdown.transform.position = new Vector3(x+50, y, 0);
-        dropdown.gameObject.SetActive(true);
-
-        dropdownRectClosed = new Rect(x, y, 160, 30);
-        dropdownRectClosed = new Rect(x, y, 160, 90);
+        dropdown.transform.position = new Vector3(x, y, 0);
+        dropdown.SetActive(true);
     }
 
-   
+    override public void OnPointerExit(PointerEventData data)
+    {
+        Hide();
+    }
 }
---
+++ agricola-unity/Assets/MainScene/Scripts/GUI/OptionScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class OptionScript : EventTrigger
+{
+    override public void OnPointerEnter(PointerEventData data)
+    {
+        GetComponent<Image>().color = Color.gray;
+    }
+    override public void OnPointerExit(PointerEventData data)
+    {
+        GetComponent<Image>().color = Color.white;
+    }
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        GetComponent<Image>().color = Color.white;
+        FindObjectOfType<DropdownSelect>().OnOptionSelected(transform.GetChild(0).GetComponent<Text>().text);
+    }
+}
